9f875d49811845a979e01ce0e160bc764455148d
tfermoso <tomas.fermoso@gmail.com>
2025-01-16 16:49:24 +0100

    registro.php

---
+++ registro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" id="nombre" required placeholder="Nombre">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" required placeholder="Email">
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" id="password" required placeholder="Contraseña">
+        <input type="submit" value="Registrarse">
+    </form>
+</body>
+</html>
