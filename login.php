68a2f2138927c3ca21c06de1c3417d939dd2db60
tfermoso <tomas.fermoso@gmail.com>
2025-01-16 18:29:26 +0100

    login

--- login.php
+++ login.php
@@ -1,3 +1,25 @@
+<?php
+if(isset($_POST["email"])){
+   include("conexiondb.php");
+    $sql="SELECT * FROM usuarios WHERE email=:email";
+    $stm=$conexion->prepare($sql);
+    $stm->bindParam(":email", $_POST["email"]); 
+    $stm->execute();
+    $usuario=$stm->fetch();
+    if($usuario){
+        if(password_verify($_POST["password"], $usuario["password"])){
+            echo "Bienvenido";
+            exit();
+        }else{
+            echo "Contraseña incorrecta";
+            exit();
+        } 
+    }else{
+        echo "Usuario no encontrado";
+        exit();
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +28,12 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="" method="post">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" required placeholder="Email">
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" id="password" required placeholder="Contraseña">
+        <input type="submit" value="Iniciar sesión">
+    </form>
 </body>
 </html>
